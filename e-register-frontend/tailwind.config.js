b9aac4e4e214fc111aee64a921be60f041489c55
Henry Emeka Obadoni <47888360+jediiry@users.noreply.github.com>
2022-09-20 19:45:19 +0100

    fix: updated

---
+++ e-register-frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  prefix: "tw-",
+  future: {},
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        "dark-yellow": "#D7A300",
+        "dark-green": "#076476",
+        "light-green": "#50C18D",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
