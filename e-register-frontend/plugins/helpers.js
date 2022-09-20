b63b0e256b3ff3b30aa69172decd0544e76028e1
Henry Emeka Obadoni <47888360+jediiry@users.noreply.github.com>
2022-09-20 20:53:50 +0100

    fix: updated

--- e-register-frontend/plugins/helpers.js
+++
@@ -1,9 +0,0 @@
-export default (context, inject) => {
-  const throwError = (err) => {
-    err?.message
-      ? context.$toast.error(err.message)
-      : context.$toast.error(err?.data?.message ?? "An error occurred");
-  };
-
-  inject("throwError", throwError);
-};
