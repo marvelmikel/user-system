36c26084751bac523d477843cd01d879c6a24332
Henry Emeka Obadoni <47888360+jediiry@users.noreply.github.com>
2022-09-22 11:45:24 +0100

    fix: updated

--- e-register-frontend/nuxt.config.js
+++ e-register-frontend/nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ["~/css/font.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ["~/plugins/axios", "~/plugins/helpers"],
