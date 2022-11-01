57b49c486f193ebb8df2be57adf2bc07dd1dd827
Henry Emeka Obadoni <47888360+jediiry@users.noreply.github.com>
2022-11-01 17:31:10 +0100

    fix: updated

--- e-register-backend/src/accreditation/entities/accreditation.entity.ts
+++ e-register-backend/src/accreditation/entities/accreditation.entity.ts
@@ -15,15 +15,15 @@ export class Accreditation {
   id: string;
 
   @ObjectIdColumn()
-  @Field(() => String, { defaultValue: null, nullable: true })
+  @Field(() => ID, { defaultValue: null, nullable: true })
   categoryId: string;
 
   @ObjectIdColumn()
-  @Field(() => String, { defaultValue: null, nullable: true })
+  @Field(() => ID, { defaultValue: null, nullable: true })
   subCategoryId: string;
 
   @ObjectIdColumn()
-  @Field(() => String, { defaultValue: null, nullable: true })
+  @Field(() => ID, { defaultValue: null, nullable: true })
   userId: string;
 
   @ObjectIdColumn()
