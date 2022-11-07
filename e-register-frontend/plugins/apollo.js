7239555a2d114122473c7a91ed861828e7da088c
chiddy50 <codewelldev@gmail.com>
2022-11-07 22:49:19 +0100

    Updated

--- e-register-frontend/plugins/apollo.js
+++ e-register-frontend/plugins/apollo.js
@@ -1,29 +1,39 @@
 import { onError } from 'apollo-link-error'
 
 export default (context) => {
+  console.log(context.route);
   const errorLink = onError(({ graphQLErrors, networkError }) => {
     let errors = graphQLErrors ? [...graphQLErrors] : [];
 
     if (errors.length > 0) {
-      errors.forEach(error => {
-          if (error.message == 'Invalid token') {
+      errors.forEach(({ message, locations, path }) => {
+        console.log(
+          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+        )
+        if (message == 'Invalid token') {
 
-            let path = context.route.path
-            let regex = new RegExp('/admin', "i");
-            console.log(regex.test(path) ? `admin: ${path} route` : `user: ${path} route`);
+          let path = context.route.path
+          let regex = new RegExp('/admin', "i");
+          console.log(regex.test(path) ? `admin: ${path} route` : `user: ${path} route`);
 
-            if (regex.test(path)) {
-              context.app.store.commit("ADMIN_LOGOUT");
-              context.app.router.push("/admin/signin");
-              context.$toast.error("Expired Token");
-            }else{
-              context.app.store.commit("LOGOUT");
-              context.app.router.push("/signin");
-              context.$toast.error("Expired Token");
-            }
+          ;
+
+          if (path.indexOf("admin") === 1) {
+            context.app.store.commit("ADMIN_LOGOUT");
+            context.app.router.push("/admin/signin");
+          }else{
+            context.app.store.commit("LOGOUT");
+            context.app.router.push("/signin");
           }
+          context.$toast.error("Expired Token");
+        }
       });
     }
+
+    if (networkError) {
+      console.log(`[Network error]: ${networkError}`)
+    }
+
     console.log('graphQLErrors', errors);
     console.log('networkError', networkError);
   })
--- e-register-frontend/plugins/helpers.js
+++ e-register-frontend/plugins/helpers.js
@@ -5,14 +5,17 @@ export default (context, inject) => {
       //   const { networkError, message, gqlError, graphqlErrors } = graphqlError;
       // console.log(err);
       if (errors.length > 0) {
-        errors.forEach(error => {
-            if (error.message == 'Invalid token' || error.message == 'jwt expired') {
+        errors.forEach(({ message, locations, path }) => {
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+            if (message == 'Invalid token' || message == 'jwt expired') {
 
               let path = context.route.path
               let regex = new RegExp('/admin', "i");
               console.log(regex.test(path) ? `admin: ${path} route` : `user: ${path} route`);
 
-              if (regex.test(path)) {
+              if (path.indexOf("admin") === 1) {
                 context.app.store.commit("ADMIN_LOGOUT");
                 context.app.router.push("/admin/signin");
                 context.$toast.error("Expired Token");
